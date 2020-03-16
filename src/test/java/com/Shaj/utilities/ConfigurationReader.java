705c927b23e4ebd3b1265227fee472aa7611b79d
Shahrizat Rosul <ap-in@LT0416.apin.com>
2020-03-16 18:51:03 -0400

    shaj project_utility package

---
+++ src/test/java/com/Shaj/utilities/ConfigurationReader.java
@@ -0,0 +1,30 @@
+package com.Shaj.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties;
+
+    static {
+        String path="Configuration.properties";
+
+        try {
+            FileInputStream fileInputStream=new FileInputStream(path);
+
+            properties=new Properties();
+            properties.load(fileInputStream);
+            fileInputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getProperty(String property){
+        return properties.getProperty(property);
+    }
+}
